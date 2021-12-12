4795c7b9e5a4341db17a253fdc9645ea4b51e400
Hudsson86 <89141541+Hudsson86@users.noreply.github.com>
2021-12-12 18:07:22 -0400

    Delete cadastros directory

--- cadastros/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import update_last_login
-from .models import Vaga
-
-
-@admin.register(Vaga)
-
-class VagaAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'created', 'updated')
